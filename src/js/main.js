f9357256dae9864cbfdaa7cb78bf50f67154427c
Sebastian Agudelo <sebastianagudelo95@hotmail.com>
2024-12-07 19:35:15 +0100

    aa

--- src/js/main.js
+++ src/js/main.js
@@ -3,17 +3,20 @@ import "./../scss/main.scss";
 import { burgerMenuFunction } from "./module/burbermenu.js";
 import { allCartListeners } from "./module/cart.js";
 import { fetchData } from "./fetch.js";
-// 
+// import {products} from "./module/products.js"
+
 
 window.onload = () => {
+  console.log('Page Loaded:', location.pathname);  // Lägg till logg
   if (location.pathname.includes("index.html")) {
     const products = document.querySelector("#productContainer");
     if (products) {
-      products.style.display = "none" 
+      products.style.display = "none"; 
     }
   }
-  
+
   if (location.pathname.includes("products.html")) {
+    console.log("Fetching data for products page");
     fetchData();
     const heroImg = document.querySelector(".hero-img");
     if (heroImg) {
@@ -22,6 +25,7 @@ window.onload = () => {
   }
 
 }
+// products()
 
 
 //Open and close meny
