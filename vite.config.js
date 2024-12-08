8f4e8d903602fa28a18876866b3d97cc00bf4f2c
Sebastian Agudelo <sebastianagudelo95@hotmail.com>
2024-12-08 19:13:18 +0100

    aa

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/WEBSHOP%20HEADPHONES/', // Här anger du din app's base path (med %20 för mellanslag)
   build: {
     rollupOptions: {
       input: {
         index: './index.html',
         products: './products.html',
-      },
-    },
-  },
+      }
+    }
+  }
 });
